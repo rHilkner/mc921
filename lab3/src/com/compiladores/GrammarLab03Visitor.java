ccc72dbc314d046d74129b4a097d997798f7aa20
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2019-09-11 21:16:00 -0300

    Renamed all variables to GrammarLab03

--- lab3/src/com/compiladores/GrammarLab03Visitor.java
+++ lab3/src/com/compiladores/GrammarLab03Visitor.java
@@ -1,17 +1,17 @@
 package com.compiladores;
 
 public class GrammarLab03Visitor {
-    public Integer visitRoot(SummerParser.RootContext ctx)
+    public Integer visitRoot(GrammarLab03Parser.RootContext ctx)
     {
         Integer val = visit(ctx.expr(0)) + visit(ctx.expr(1));
         System.out.println(ctx.ADD().getText() + val);
         return 0;
     }
-    public Integer visitExprSum(SummerParser.ExprSumContext ctx)
+    public Integer visitExprSum(GrammarLab03Parser.ExprSumContext ctx)
     {
         return visit(ctx.expr(0)) + visit(ctx.expr(1));
     }
-    public Integer visitExprNum(SummerParser.ExprNumContext ctx)
+    public Integer visitExprNum(GrammarLab03Parser.ExprNumContext ctx)
     {
         return Integer.valueOf(ctx.NUM().getText());
     }
