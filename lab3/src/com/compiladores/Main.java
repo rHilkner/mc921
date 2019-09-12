0a34bb97729cd1d16068f28c861dad9af86cd092
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2019-09-11 21:30:22 -0300

    Filled main func

--- lab3/src/com/compiladores/Main.java
+++ lab3/src/com/compiladores/Main.java
@@ -3,6 +3,16 @@ package com.compiladores;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // create a CharStream that reads from standard input
+        CharStream input = CharStreams.fromStream(System.in);
+        // create a lexer that feeds off of input CharStream
+        GrammarLab03Lexer lexer = new GrammarLab03Lexer(input);
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // create a parser that feeds off the tokens buffer
+        GrammarLab03Parser parser = new GrammarLab03Parser(tokens);
+        ParseTree tree = parser.root(); // begin parsing at prog rule
+        AddVisitor vis = new AddVisitor();
+        vis.visit(tree);
     }
 }
--- lab3/src/com/compiladores/script.sh
+++ lab3/src/com/compiladores/script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -x
-#compiling the Sum.g4 in java files
-java -jar "antlr-4.7.2-complete.jar" -no-listener -visitor GrammarLab03.g4
-#exporting the class path
-export CLASSPATH=".:antlr-4.7.2-complete.jar:$CLASSPATH"
+##compiling the Sum.g4 in java files
+#java -jar "antlr-4.7.2-complete.jar" -no-listener -visitor GrammarLab03.g4
+##exporting the class path
+#export CLASSPATH=".:antlr-4.7.2-complete.jar:$CLASSPATH"
 #compiling the .java generated from Sum.g4 with MyParser.java and AddVisitor.java
-#javac *.java
-##feeding a string and reading the tokens
-#echo "(5+4+3+2+10)" | java org.antlr.v4.gui.TestRig GrammarLab03 root -tokens
-##feeding a string and reading tree in list style
-#echo "(5+4+3+2+10)" | java org.antlr.v4.gui.TestRig GrammarLab03 root -tree
-##feeding a string and printing a graphical tree
-#echo "(5+4+3+2+10)" | java org.antlr.v4.gui.TestRig GrammarLab03 root -gui
-##execute the implemented visitor
-#echo "(5+4+3+2+1)" | java GrammarLab03Parser
+javac *.java
+#feeding a string and reading the tokens
+echo "var a = 0;" | java org.antlr.v4.gui.TestRig GrammarLab03 root -tokens
+#feeding a string and reading tree in list style
+echo "func a(x) 2 + x * 5;" | java org.antlr.v4.gui.TestRig GrammarLab03 root -tree
+#feeding a string and printing a graphical tree
+echo "var a = 0;" | java org.antlr.v4.gui.TestRig GrammarLab03 root -gui
+#execute the implemented visitor
+echo "var a = 0;" | java GrammarLab03Parser
