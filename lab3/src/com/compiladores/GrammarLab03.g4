18212dd3c0d279bf01542ab46cc0c2326d62c49e
Wilson Martins <wilson@kissolutions.tech>
2019-10-03 11:07:22 -0300

    Feature: lab 4 initial changes

--- lab3/src/com/compiladores/GrammarLab03.g4
+++ lab3/src/com/compiladores/GrammarLab03.g4
@@ -6,20 +6,20 @@ func: 'func ' ID ('('|' (') ID?(', 'ID)*') ' expr;
 
 var: 'var ' ID ' = ' expr;
 
-expr: expr '*' expr                                 # ExprMult
-    | expr '/' expr                                 # ExprDiv
-    | expr '+' expr                                 # ExprSum
-    | expr '-' expr                                 # ExprSub
-    | '('expr')'                                    # ExprParenthesis
-    | ' ('expr')'                                   # ExprParenthesisSpaceLeft
-    | '('expr') '                                   # ExprParenthesisSpaceRight
-    | ' ('expr') '                                  # ExprParenthesisSpaceBoth
-    | ' 'expr' '                                    # ExprSpaceBoth
-    | expr' '                                       # ExprSpaceRight
-    | ' 'expr                                       # ExprSpaceLeft
+expr: expr '*' expr                                                     # ExprMult
+    | expr '/' expr                                                     # ExprDiv
+    | expr '+' expr                                                     # ExprSum
+    | expr '-' expr                                                     # ExprSub
+    | '('expr')'                                                        # ExprParenthesis
+    | ' ('expr')'                                                       # ExprParenthesisSpaceLeft
+    | '('expr') '                                                       # ExprParenthesisSpaceRight
+    | ' ('expr') '                                                      # ExprParenthesisSpaceBoth
+    | ' 'expr' '                                                        # ExprSpaceBoth
+    | expr' '                                                           # ExprSpaceRight
+    | ' 'expr                                                           # ExprSpaceLeft
     | ID ('('|' (') (ID | NUM)? ((', '|',') (ID | NUM))* (')'|') ')     # ExprFuncCall
-    | NUM                                           # ExprNum
-    | ID                                            # ExprId
+    | NUM                                                               # ExprNum
+    | ID                                                                # ExprId
     ;
 
 ID : [a-zA-Z_][a-zA-Z0-9_]*;
--- lab3/src/com/compiladores/MyAddVisitor.java
+++ lab3/src/com/compiladores/MyAddVisitor.java
@@ -2,38 +2,53 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
-public class MyAddVisitor extends GrammarLab03BaseVisitor<Integer> {
+public class MyAddVisitor extends GrammarLab03BaseVisitor<String> {
 
     private Map<String, String> typeVarMap = new HashMap<>();
-    private Map<String, Integer> funcArgsMap = new HashMap<>();
+    private Map<String, Integer> funcArgsCount = new HashMap<>();
     private List<String> funcVars = new ArrayList<>();
 
     private final String VAR = "var";
     private final String FUNC = "func";
 
+    private AtomicInteger tempVarCount = new AtomicInteger(1);
+
     @Override
-    public Integer visitRoot(GrammarLab03Parser.RootContext ctx) {
+    public String visitRoot(GrammarLab03Parser.RootContext ctx) {
         return visitChildren(ctx);
     }
 
     @Override
-    public Integer visitVar(GrammarLab03Parser.VarContext ctx) {
+    public String visitVar(GrammarLab03Parser.VarContext ctx) {
         String varToken = ctx.ID().getText();
         if (typeVarMap.containsKey(varToken)) {
             System.out.println("Symbol already declared: " + varToken);
-            return 1;
+            return "ERROR 84";
         }
         typeVarMap.put(varToken, VAR);
-        return visit(ctx.expr());
+
+        if (ctx.expr().children.size() == 1) {
+            System.out.println("@" + varToken + " = global i32 " + visit(ctx.expr()) + ";");
+        } else {
+            String funcName = "f" + tempVarCount.getAndIncrement();
+            System.out.println("define i32 @" + funcName + "(" + ") {");
+            visit(ctx.expr());
+            System.out.println("ret i32 %v" + (tempVarCount.get()-1));
+            System.out.println("}");
+            System.out.println("@" + varToken + " = call i32 @" + funcName + "(" + ")");
+        }
+        return null;
     }
 
     @Override
-    public Integer visitFunc(GrammarLab03Parser.FuncContext ctx) {
+    public String visitFunc(GrammarLab03Parser.FuncContext ctx) {
         String funcToken = ctx.ID(0).getText();
         if (typeVarMap.containsKey(funcToken)) {
             System.out.println("Symbol already declared: " + funcToken);
-            return 1;
+            return "ERROR 85";
         }
         typeVarMap.put(funcToken, FUNC);
         for (int i = 1; i < ctx.ID().size(); i++) {
@@ -44,17 +59,20 @@ public class MyAddVisitor extends GrammarLab03BaseVisitor<Integer> {
 //                return 1;
 //            }
         }
-        funcArgsMap.put(funcToken, ctx.ID().size() - 1);
+        funcArgsCount.put(funcToken, ctx.ID().size() - 1);
 //        System.out.println("Teste1: " + ctx);
 //        System.out.println("Teste2: " + ctx.expr());
 
+        System.out.println("define i32 @" + funcToken + "(" + this.listToParams(funcVars) + ") {");
         visit(ctx.expr());
+        System.out.println("ret i32 %v" + (tempVarCount.get()-1));
+        System.out.println("}");
         funcVars.clear();
-        return 1;
+        return null;
     }
 
     @Override
-    public Integer visitExprId(GrammarLab03Parser.ExprIdContext ctx) {
+    public String visitExprId(GrammarLab03Parser.ExprIdContext ctx) {
         String idToken = ctx.ID().getText();
         if (typeVarMap.getOrDefault(idToken, VAR).equals(FUNC) && !funcVars.contains(idToken)) {
             System.out.println("Bad used symbol: " + idToken);
@@ -62,20 +80,39 @@ public class MyAddVisitor extends GrammarLab03BaseVisitor<Integer> {
         if (!typeVarMap.containsKey(idToken) && !funcVars.contains(idToken)) {
             System.out.println("Symbol undeclared: " + idToken);
         }
-        return 1;
+        if (typeVarMap.containsKey(idToken)) {
+            return "@" + idToken;
+        } else {
+            return  "%" + idToken;
+        }
     }
 
     @Override
-    public Integer visitExprFuncCall(GrammarLab03Parser.ExprFuncCallContext ctx) {
+    public String visitExprSpaceBoth(GrammarLab03Parser.ExprSpaceBothContext ctx) {
+        return visit(ctx.children.get(1));
+    }
+
+    @Override
+    public String visitExprSpaceRight(GrammarLab03Parser.ExprSpaceRightContext ctx) {
+        return visit(ctx.children.get(0));
+    }
+
+    @Override
+    public String visitExprSpaceLeft(GrammarLab03Parser.ExprSpaceLeftContext ctx) {
+        return visit(ctx.children.get(1));
+    }
+
+    @Override
+    public String visitExprFuncCall(GrammarLab03Parser.ExprFuncCallContext ctx) {
         String funcToken = ctx.ID(0).getText();
         if (!typeVarMap.containsKey(funcToken)) {
             System.out.println("Symbol undeclared: " + funcToken);
-            return 1;
+            return "ERROR 87";
         }
 
         if (typeVarMap.get(funcToken).equals(VAR)) {
             System.out.println("Bad used symbol: " + funcToken);
-            return 1;
+            return "ERROR 88";
         }
 
         int funcArgsCount = 0;
@@ -87,96 +124,147 @@ public class MyAddVisitor extends GrammarLab03BaseVisitor<Integer> {
             funcArgsCount += ctx.NUM().size();
         }
 
-        if (funcArgsMap.get(funcToken) != null && funcArgsMap.get(funcToken) != funcArgsCount) {
-            System.out.println("Bad argument count: " + funcToken);
-            return 1;
+//        if (this.funcArgsCount.get(funcToken) != null && this.funcArgsCount.get(funcToken) != funcArgsCount) {
+//            System.out.println("Bad argument count: " + funcToken);
+//            return "ERROR 89";
+//        }
+
+        List<String> funcArguments = new ArrayList<>();
+        List<String> funcCallChildren = ctx.children.stream().map(x -> x.getText()).filter(x -> !x.contains("(") && !x.contains(")") && !x.contains(",")).collect(Collectors.toList());
+        for (int i = 1; i < funcCallChildren.size(); i++) {
+            String idToken = funcCallChildren.get(i);
+
+//            if (!typeVarMap.containsKey(idToken) && !funcVars.contains(idToken)) {
+//                System.out.println("Symbol undeclared: " + idToken);
+//                return "ERROR 90";
+//            }
+//
+//            if (FUNC.equals(typeVarMap.get(idToken))) {
+//                System.out.println("Bad used symbol: " + idToken);
+//                return "ERROR 91";
+//            }
+            funcArguments.add(idToken);
+
         }
 
-
-        for (int i = 1; i < ctx.ID().size(); i++) {
-            String idToken = ctx.ID(i).getText();
-
-            if (!typeVarMap.containsKey(idToken) && !funcVars.contains(idToken)) {
-                System.out.println("Symbol undeclared: " + idToken);
-                return 1;
-            }
-
-            if (FUNC.equals(typeVarMap.get(idToken))) {
-                System.out.println("Bad used symbol: " + idToken);
-                return 1;
-            }
-        }
-
-        return 1;
+        String tempVar = "%v" + this.tempVarCount.getAndIncrement();
+        System.out.println(tempVar + " = call i32 @" + funcToken + "(" + this.listToParams(funcArguments) + ")");
+        return tempVar;
     }
 
     @Override
-    public Integer visitExprNum(GrammarLab03Parser.ExprNumContext ctx) {
-        return Integer.valueOf(ctx.NUM().getText());
+    public String visitExprNum(GrammarLab03Parser.ExprNumContext ctx) {
+        return ctx.NUM().getText();
     }
 
     @Override
-    public Integer visitExprDiv(GrammarLab03Parser.ExprDivContext ctx) {
+    public String visitExprDiv(GrammarLab03Parser.ExprDivContext ctx) {
         if (ctx != null && ctx.expr() != null) {
-            Integer v1 = visit(ctx.expr(0));
-            Integer v2 = visit(ctx.expr(1));
-            if (v1 != null && v2 != null && v2 != 0) {
-                return v1 / v2;
-            } else {
-                return 1;
-            }
-        } else {
-            return 1;
-        }
-    }
-
-    @Override
-    public Integer visitExprMult(GrammarLab03Parser.ExprMultContext ctx) {
-        if (ctx != null && ctx.expr() != null) {
-            Integer v1 = visit(ctx.expr(0));
-            Integer v2 = visit(ctx.expr(1));
+            String v1 = visit(ctx.expr(0));
+            String v2 = visit(ctx.expr(1));
             if (v1 != null && v2 != null) {
-                return v1 * v2;
+                int tempVar = tempVarCount.getAndIncrement();
+                System.out.println("%v" + tempVar + " = div i32 " + v1 + ", " + v2);
+                return "%v" + tempVar;
             } else {
-                return 1;
+                return "ERROR 93";
             }
         } else {
-            return 1;
+            return "ERROR 94";
         }
     }
 
     @Override
-    public Integer visitExprSum(GrammarLab03Parser.ExprSumContext ctx) {
+    public String visitExprMult(GrammarLab03Parser.ExprMultContext ctx) {
         if (ctx != null && ctx.expr() != null) {
-            Integer v1 = visit(ctx.expr(0));
-            Integer v2 = visit(ctx.expr(1));
+            String v1 = visit(ctx.expr(0));
+            String v2 = visit(ctx.expr(1));
             if (v1 != null && v2 != null) {
-                return v1 + v2;
+                int tempVar = tempVarCount.getAndIncrement();
+                System.out.println("%v" + tempVar + " = mul i32 " + v1 + ", " + v2);
+                return "%v" + tempVar;
             } else {
-                return 1;
+                return "ERROR 95";
             }
         } else {
-            return 1;
+            return "ERROR 96";
         }
     }
 
     @Override
-    public Integer visitExprSub(GrammarLab03Parser.ExprSubContext ctx) {
+    public String visitExprSum(GrammarLab03Parser.ExprSumContext ctx) {
         if (ctx != null && ctx.expr() != null) {
-            Integer v1 = visit(ctx.expr(0));
-            Integer v2 = visit(ctx.expr(1));
+            String v1 = visit(ctx.expr(0));
+            String v2 = visit(ctx.expr(1));
             if (v1 != null && v2 != null) {
-                return v1 - v2;
+                int tempVar = tempVarCount.getAndIncrement();
+                System.out.println("%v" + tempVar + " = add i32 " + v1 + ", " + v2);
+                return "%v" + tempVar;
             } else {
-                return 1;
+                return "ERROR 97";
             }
         } else {
-            return 1;
+            return "ERROR 98";
         }
     }
 
     @Override
-    public Integer visitExprParenthesis(GrammarLab03Parser.ExprParenthesisContext ctx) {
+    public String visitExprSub(GrammarLab03Parser.ExprSubContext ctx) {
+        if (ctx != null && ctx.expr() != null) {
+            String v1 = visit(ctx.expr(0));
+            String v2 = visit(ctx.expr(1));
+            if (v1 != null && v2 != null) {
+                int tempVar = tempVarCount.getAndIncrement();
+                System.out.println("%v" + tempVar + " = sub i32 " + v1 + ", " + v2);
+                return "%v" + tempVar;
+            } else {
+                return "ERROR 99";
+            }
+        } else {
+            return "ERROR 100";
+        }
+    }
+
+    @Override
+    public String visitExprParenthesis(GrammarLab03Parser.ExprParenthesisContext ctx) {
         return visitChildren(ctx);
+    }
+
+    @Override
+    public String visitExprParenthesisSpaceLeft(GrammarLab03Parser.ExprParenthesisSpaceLeftContext ctx) {
+        return visitChildren(ctx);
+    }
+
+    @Override
+    public String visitExprParenthesisSpaceRight(GrammarLab03Parser.ExprParenthesisSpaceRightContext ctx) {
+        return visit(ctx.children.get(1));
+    }
+
+    @Override
+    public String visitExprParenthesisSpaceBoth(GrammarLab03Parser.ExprParenthesisSpaceBothContext ctx) {
+        return visitChildren(ctx);
+    }
+
+    private String listToParams(List<String> l) {
+        if (l == null || l.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < l.size(); i++) {
+            String token = l.get(i);
+            if (!token.matches("\\d+")) {
+                if (typeVarMap.containsKey(token)) {
+                    token = "@" + token;
+                } else {
+                    token = "%" + token;
+                }
+            }
+            strBuilder = strBuilder.append("i32 " + token);
+            if (i < l.size() - 1) {
+                strBuilder = strBuilder.append(", ");
+            }
+        }
+        return strBuilder.toString();
     }
 }
